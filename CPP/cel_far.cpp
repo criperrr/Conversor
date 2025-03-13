b9587221ae640572e8f46ae78e791365f8ec6376
Gabriel <161367649+criperrr@users.noreply.github.com>
2025-03-13 17:28:00 -0300

    no

--- CPP/cel_far.cpp
+++ CPP/cel_far.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     float temp;
-    cout << "Escreva a temperatura em fahrenheit : ";
+    cout << "Escreva a temperatura em Celcius : ";
     while(!(cin>>temp)){
         cout << "Escreva um valor inteiro ou decimal" << endl;
         std::cin.clear();
         std::cin.ignore(20, '\n');
 
     }
-    cout << "O valor em celcius e: " << ((temp-32.0)*5.0)/9.0 << " C" << endl;
+    cout << "O valor em Fahrenheit e: " << (temp*1.8)+32 << " F" << endl;
     return 0;
 }   
