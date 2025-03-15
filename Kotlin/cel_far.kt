d71b7c676e9662efc3b2781da6ba2036ddbfdfce
Henrique Claro <henrique.claro@unesp.br>
2025-03-15 00:07:20 -0300

    Kotlin: adicionado conversor em Kotlin

---
+++ Kotlin/cel_far.kt
@@ -0,0 +1,14 @@
+fun main(){
+    print("- Insira a temperatura em Celsius: ")
+    var tempC = 0f
+    while(true){
+        try {
+            tempC = readln().toFloat()
+            break
+        }
+        catch(e: NumberFormatException) {
+            print("- Insira um valor inteiro ou decimal: ")
+        }
+    }
+    println("- A temperatura em Fahrenheit é: %.2f".format(tempC * 9/5 + 32))
+}
