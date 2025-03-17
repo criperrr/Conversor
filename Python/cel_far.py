4c961feb35d1be7cd28c7a577aaf4febcf881a04
Henrique Claro <henrique.claro@unesp.br>
2025-03-16 23:40:23 -0300

    Python: código aprimorado, com opções

--- Python/cel_far.py
+++ Python/cel_far.py
@@ -1,9 +1,25 @@
-tempC = input("- Insira a temperatura em Celsius: ")
-while True:
-    try:
-        tempC = float(tempC)
-        break
-    except ValueError:
-        tempC = input("- Insira um valor inteiro ou decimal: ")
-tempF = (tempC * 9/5) + 32 # Temperatura em Fahrenheit
-print(f"\n- A temperatura em Fahrenheit é: {tempF:.2f}")
+def converterTipo(x): # Verificação e conversão do tipo da variável
+    while True:
+        try:
+            x = float(x)
+            break
+        except ValueError:
+            x = input("- Insira um valor inteiro ou decimal: ")
+    return x
+
+
+base = input("- A temperatura base está em graus Celsius(C) ou Fahrenheit(F)?\n> ").lower()
+while base != 'c' and base != 'f': # Consistência
+    base = input("- Insira uma opção válida: ").lower()
+
+if base == 'c':
+    tempC = input("- Insira a temperatura em graus Celsius: ")
+    tempC = converterTipo(tempC)
+    tempF = (tempC * 9/5) + 32 # Temperatura em Fahrenheit
+    
+else:
+    tempF = input("- Insira a temperatura em graus Fahrenheit: ")
+    tempF = converterTipo(tempF)
+    tempC = (tempF - 32) * 5/9 # Temperatura em Celsius
+
+print(f"\n- A temperatura em graus Fahrenheit é: {tempF:.2f}") if base == 'c' else print(f"\n- A temperatura em graus Celsius é: {tempC:.2f}")
