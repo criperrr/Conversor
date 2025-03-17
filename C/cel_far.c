05eb4abbd252453d3926f528facb8bf1a37cb5d4
Henrique Claro <henrique.claro@unesp.br>
2025-03-17 01:17:05 -0300

    C: código aprimorado

--- C/cel_far.c
+++ C/cel_far.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <locale.h>
+#include <string.h>
 
 int main(){
-    float tempC; //Temperatura Celsius
-    printf("- Insira a temperatura em graus Celsius:\n> ");
-    scanf("%f", &tempC);
-    printf("\n- Temperatura em Fahrenheit: %.2f", (tempC * 9/5) + 32);
+    setlocale(LC_ALL,"");
+
+    char base[2]; //String pra poder usar strlwr(), só se checa o [0]
+    printf("- A temperatura base está em graus Celsius(C) ou Fahrenheit(F)?\n> ");
+    gets(base);
+    strlwr(base);
+    fflush(stdin);
+    while (base[0] != 'c' && base[0] != 'f') { //Consistência
+        printf("- Insira uma opção válida: ");
+        fflush(stdin);
+        gets(base);
+        strlwr(base);
+    }
+    
+    float tempC, tempF;
+    if (base[0] == 'c') {
+        printf("- Insira a temperatura em graus Celsius:\n> ");
+        scanf("%f", &tempC);
+        printf("\n- Temperatura em graus Fahrenheit: %.2f", (tempC * 9/5) + 32); //Celcius -> Fahrenheit
+    }
+    else {
+        printf("- Insira a temperatura em graus Fahrenheit\n> ");
+        scanf("%f", &tempF);
+        printf("\n- Temperatura em graus Celsius: %.2f", (tempF - 32) * 5/9); //Fahrenheit -> Celsius
+    }
+
     return 0;
 }
