a6e032eda7512b501c4b9f9861c57bb454cb315a
Henrique Claro <henrique.claro@unesp.br>
2025-03-17 20:41:41 -0300

    C: consistência melhorada

--- C/cel_far-kel.c
+++ C/cel_far-kel.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+
+float converterTipo(float x) { //Consistência de tipo usando o valor de retorno do scanf()
+    if (scanf("%f", &x) != 1) {
+        printf("- Insira um valor inteiro ou decimal: ");
+        while(getchar() != '\n'); //Limpa o stdin (coisa que o fflush não consegue)
+        converterTipo(x);
+    }
+    else {
+        return x;
+    }
+}
 
 void imprimir(char *base, float tempC){ //Mostra os valores convertidos
     if (base[0] == 'c' || base[0] == 'k')
@@ -16,46 +27,45 @@ void imprimir(char *base, float tempC){ //Mostra os valores convertidos
 int main(){
     setlocale(LC_ALL,"");
 
-    char base[2]; //String pra poder usar strlwr(), só se checa o [0]
+    char *base = (char*) malloc(2 * (sizeof(char))); //String pra poder usar strlwr(), só se checa o [0]
     printf("- A temperatura base está em graus Celsius(C), Fahrenheit(F) ou Kelvin(K)?\n> ");
-    gets(base);
-    strlwr(base);
+    fgets(base, 2, stdin);
     fflush(stdin);
+    strlwr(base);
 
     float tempC, tempF, tempK;
-    bool verify; //Bool pra fazer a consistência
-    do {
-        verify = false;
-        switch (base[0]) {
-            case 'c':
-                printf("- Insira a temperatura em graus Celsius:\n> ");
-                scanf("%f", &tempC);
-                imprimir(base, tempC);
-                break;
+   
+    Casos: //Casos possíveis, tbm serve como ponto de consistência (goto)
+    switch (base[0]) {
+        case 'c':
+            printf("- Insira a temperatura em graus Celsius:\n> ");
+            tempC = converterTipo(tempC);
+            imprimir(base, tempC);
+            break;
             
-            case 'f':
-                printf("- Insira a temperatura em graus Fahrenheit\n> ");
-                scanf("%f", &tempF);
-                tempC = (tempF - 32) * 5/9;
-                imprimir(base, tempC);
-                break;
+        case 'f':
+            printf("- Insira a temperatura em graus Fahrenheit\n> ");
+            tempF = converterTipo(tempF);
+            tempC = (tempF - 32) * 5/9;
+            imprimir(base, tempC);
+            break;
             
-            case 'k':
-                printf("- Insira a temperatura em Kelvin:\n> ");
-                scanf("%f", &tempK);
-                tempC = tempK - 273.15;
-                imprimir(base, tempC);
-                break;
+        case 'k':
+            printf("- Insira a temperatura em Kelvin:\n> ");
+            tempK = converterTipo(tempK);
+            tempC = tempK - 273.15;
+            imprimir(base, tempC);
+            break;
 
-            default:
-                printf("- Insira uma opção válida: ");
-                fflush(stdin);
-                gets(base);
-                strlwr(base);
-                verify = true;
-                break;
-        }
-    } while (verify); //Consistência
-
+        default:
+            printf("- Insira uma opção válida: ");
+            fgets(base, 2, stdin);
+            fflush(stdin);
+            strlwr(base);
+            goto Casos;
+            break;
+    }
+    
+    free(base);
     return 0;
 }
